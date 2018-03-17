02dca9a4f95e322944bc2cb8d69084d925bf8b72
bri <briegrant416@gmail.com>
2018-03-17 10:28:04 +0300

    Add registrattion folder with base.html details

--- watch/views.py
+++ watch/views.py
@@ -4,3 +4,16 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
